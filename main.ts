c7abae61f65ec7dfe34310eb7cd7953a0dd358ca
antz300 <122234682+antz300@users.noreply.github.com>
2023-01-08 21:25:49 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    stembit_Robot.Music_Car(stembit_Robot.enMusic.entertainer)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
